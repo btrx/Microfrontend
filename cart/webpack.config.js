306b0e9418e2f9b8563e45cce082dfe67f4983e3
kusumadoni77 <kusumadoni77@gmail.com>
2022-12-08 19:08:54 +0700

    Update webpack.config.js

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "https://cart-mf.vercell.app/",
+    publicPath: "https://cart-mf.vercel.app/",
   },
 
   resolve: {
